2cd79489d090c9c73420a6d78248666cfd0f9cf1
Lukas Stolz <lukastolz9@gmail.com>
2022-07-16 20:15:22 +0200

    Added lazy loading and enum to dropdown generation

--- ncrypt.Server/Model/GenericModel.cs
+++ ncrypt.Server/Model/GenericModel.cs
@@ -7,16 +7,44 @@ public class GenericModel
 {
     public String Name{ get; set; }
     public Type ModelResultType { get; set; }
-    public List<PropertyInfo> Properties { get; set; }
     public Type ServiceType { get; set; }
-    public List<MethodInfo> ServiceMethods { get; set; }
 
+    private List<PropertyInfo>? _properties;
+    public List<PropertyInfo> Properties
+    {
+        get 
+        {
+            if (_properties is null || _properties.Count == 0)
+            {
+                _properties = ModelResultType.GetProperties().ToList();
+            }
+
+            return _properties;
+        }
+    }
+
+    private List<MethodInfo>? _serviceMethods;
+    public List<MethodInfo> ServiceMethods
+    {
+        get 
+        {
+            if (_serviceMethods is null || _serviceMethods.Count == 0)
+            {
+                _serviceMethods = ServiceType.GetMethods ()
+                    .Where (m => m.CustomAttributes
+                         .Any (a => a.AttributeType == typeof (SelectableFunction))
+                    ).ToList ();
+            }
+
+            return _serviceMethods;
+        }
+    }
+        
+        
     public GenericModel (String name, Type resultType, Type serviceType)
     {
         Name = name;
         ModelResultType = resultType;
-        Properties = resultType.GetProperties().ToList();
         ServiceType = serviceType;
-        ServiceMethods = serviceType.GetMethods ().Where(m => m.CustomAttributes.Any(a => a.AttributeType == typeof(SelectableFunction))).ToList ();
     }
 }
